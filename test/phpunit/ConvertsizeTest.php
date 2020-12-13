6f29b275dc023fdb3a3da16eea853b8fc36312e6
Enjoys <deadly.info@gmail.com>
2020-12-13 21:55:07 +0300

    add test ConvertsizeTest.php

---
+++ test/phpunit/ConvertsizeTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ConvertsizeTest extends TestCase
+{
+    public function data()
+    {
+        return [
+            ['0B', 0],
+            ['10B', 10],
+            ['10K', 10 * 1024],
+            ['10M', 10 * pow(1024, 2)],
+            ['10G', 10 * pow(1024, 3)],
+            ['10T', 10 * pow(1024, 4)],
+            ['10P', 10 * pow(1024, 5)]
+        ];
+    }
+
+    /**
+     * @dataProvider  data
+     *
+     */
+    public function test_iniSize2bytes($value, $expect)
+    {
+
+        $this->assertSame($expect, \iniSize2bytes($value));
+    }
+
+    /**
+     * @dataProvider  data
+     *
+     */
+    public function test_bytes2iniSize($expect, $value)
+    {
+
+        $this->assertSame($expect, \bytes2iniSize($value));
+    }
+}
