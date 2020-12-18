ef346a49c2a71929776a89b3930dbc97d01fcbac
Enjoys <deadly.info@gmail.com>
2020-12-19 01:52:53 +0300

    min test for debug.php

---
+++ test/phpunit/debugTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Test\Enjoys\Functions;
+
+
+//use PHPUnit\Framework\TestCase;
+//
+//class debugTest extends TestCase
+//{
+//    public function test1()
+//    {
+//        $this->markTestSkipped();
+//    }
+//}
