4c9db4119f5be8b3b21aa5e13011e0d0f970e699
enjoys <deadly.info@gmail.com>
2021-05-30 23:02:36 +0300

    added removeDirectoryRecursive

--- functions/filesystem.php
+++ functions/filesystem.php
@@ -33,3 +33,27 @@ function createDirectory(string $path, int $permissions = 0777)
         }
     }
 }
+
+
+function removeDirectoryRecursive(string $path, $removeParent = false)
+{
+    $di = new \RecursiveDirectoryIterator($path, \FilesystemIterator::SKIP_DOTS);
+    $ri = new \RecursiveIteratorIterator($di, \RecursiveIteratorIterator::CHILD_FIRST);
+
+    /** @var \SplFileInfo $file */
+    foreach ($ri as $file) {
+        if ($file->isLink()) {
+            $symlink = realpath($file->getPath()) . DIRECTORY_SEPARATOR . $file->getFilename();
+            if(PHP_OS_FAMILY == 'Windows'){
+                (is_dir($symlink)) ? rmdir($symlink) : unlink($symlink);
+            }else{
+                unlink($symlink);
+            }
+            continue;
+        }
+        $file->isDir() ? rmdir($file->getRealPath()) : unlink($file->getRealPath());
+    }
+    if ($removeParent) {
+        rmdir($path);
+    }
+}
