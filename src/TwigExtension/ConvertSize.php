a5a32929073a7c21f80682075e3873a300cdde72
enjoys <deadly.info@gmail.com>
2021-12-30 21:13:17 +0300

    added twig extension ConvertSize.php

---
+++ src/TwigExtension/ConvertSize.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Enjoys\Functions\TwigExtension;
+
+
+use Twig\TwigFilter;
+
+final class ConvertSize
+{
+    public function getFilters()
+    {
+        return [
+            new TwigFilter('iniSize2bytes', [$this, 'iniSize2bytes'], ['is_safe' => ['all']]),
+            new TwigFilter('bytes2iniSize', [$this, 'bytes2iniSize'], ['is_safe' => ['all']]),
+        ];
+    }
+
+    public function iniSize2bytes($body): int
+    {
+        return iniSize2bytes($body);
+    }
+
+    public function bytes2iniSize($body): string
+    {
+        return bytes2iniSize($body);
+    }
+}
